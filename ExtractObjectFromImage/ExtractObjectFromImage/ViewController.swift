0d5c0ec46d94e81b3156007ff7fbe368f2d5ac9d
kavana <kaanand@adobe.com>
2023-07-26 12:09:14 -0700

    downsample using UIGraphicsRenderer and get the location of the first non black pixel

--- ExtractObjectFromImage/ExtractObjectFromImage/ViewController.swift
+++ ExtractObjectFromImage/ExtractObjectFromImage/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     private let visualLookup = UIButton()
     private let images = ["cow", "elephant", "people", "coffee", "street", "wineglasses"]
     private var downSampledOriginalImage : CGImage?
+    private let maxDimension = 150.0
 }
 
 extension ViewController {
@@ -189,18 +190,21 @@ extension ViewController: PHPickerViewControllerDelegate {
             }
             
             if result.itemProvider.canLoadObject(ofClass: UIImage.self) {
-                result.itemProvider.loadObject(ofClass: UIImage.self) { [self] image, error in
-                    DispatchQueue.main.async { [self] in
-                        let name = result.itemProvider.suggestedName ?? "tempImage"
-                        let object = image as! UIImage
-                        // Downsample the image to 150 as the maximum dimension for faster result
-                        guard let imageURL = createLocalURL(for: object, name: name),
-                              let dsImage = downsample(image: imageURL, to: CGSize(width: 150, height: 150)) else {
-                                  return
+                result.itemProvider.loadObject(ofClass: UIImage.self) {[self] image, error in
+                    if let image = image as? UIImage {
+                        DispatchQueue.main.async { [self] in
+//                            let name = result.itemProvider.suggestedName ?? "tempImage"
+//                            let object = image
+//                            // Downsample the image to 150 as the maximum dimension for faster result
+//                            guard let imageURL = createLocalURL(for: object, name: name),
+//                                  let dsImage = downsample(image: imageURL, to: CGSize(width: 150, height: 150)) else {
+//                                return
+//                            }
+//                            downSampledOriginalImage = dsImage
+//                            imageView.image = UIImage(cgImage: downSampledOriginalImage!)
+                            imageView.image = image
+                            maskButton.isEnabled = true
                         }
-                        downSampledOriginalImage = dsImage
-                        imageView.image = UIImage(cgImage: downSampledOriginalImage!)
-                        maskButton.isEnabled = true
                     }
                 }
             }
@@ -210,24 +214,32 @@ extension ViewController: PHPickerViewControllerDelegate {
 
 extension ViewController: ObjectRecognizerDelegate {
     func objectRecognizer(_ recognizer: ObjectRecognizer, didRecognizedAndMaskedObjects image: CGImage) {
-        for x in 0..<image.width {
-            for y in 0..<image.height {
-                let pixelData = image.dataProvider!.data
-                let dataPtr = CFDataGetBytePtr(pixelData)
-                
-                let bytesPerRow = image.bytesPerRow
-                let bytesPerPixel = image.bitsPerPixel / 8
-                let pixelOffset = y * bytesPerRow + x * bytesPerPixel
-                let value = dataPtr![pixelOffset]
-//                print(value)
-                if (value != 0) {
-                    print("(", x, ",", y , ")")
-                }
-            }
-        }
-        print("END")
+        let maskImage = UIImage(cgImage: image)
         
-        imageView.image = UIImage(cgImage: image)
+        // INFO:
+        // The following approach of downsampling is not working for grayscale cgimage
+        // The createLocalURL is loosing the actual pixel value of grayscale
+        // downsample() with CFData or CGDataProvider is not returning downsampled thumbnail image
+        /*
+        let name = "tempMaskImage"
+        let object = maskImage
+        guard let imageURL = createLocalURL(for: object, name: name),
+              let dsImage = downsample(image: imageURL, to: CGSize(width: 150, height: 150)) else {
+                  return
+        }
+        */
+        
+        let dsSize = downsampleSize(maskImage.size)
+        guard let dsImage = resize(image: maskImage, size: dsSize).cgImage else {
+            return
+        }
+        
+        let pixelLocationInDownSampledMask = getFirstVisiblePixelCoordinate(dsImage)
+        print(pixelLocationInDownSampledMask!)
+        let pixelLocation = locationInOriginalMask(size: maskImage.size, point: pixelLocationInDownSampledMask!)
+        print(pixelLocation)
+        
+        imageView.image = maskImage
         photosButton.isEnabled = true
         maskButton.isEnabled = true
     }
@@ -246,12 +258,60 @@ extension ViewController: ObjectVisualLookupDelegate {
 
 extension ViewController {
     
+    func getFirstVisiblePixelCoordinate(_ image: CGImage) -> CGPoint? {
+        for y in 0..<image.height {
+            for x in 0..<image.width {
+                let pixelData = image.dataProvider!.data
+                let dataPtr = CFDataGetBytePtr(pixelData)
+                
+                let bytesPerRow = image.bytesPerRow
+                let bytesPerPixel = image.bitsPerPixel / 8
+                let pixelOffset = y * bytesPerRow + x * bytesPerPixel
+                let value = dataPtr![pixelOffset]
+//                print(value)
+                if (value != 0) {
+                    print("(", x, ",", y , ")", " -> ", value)
+                    return CGPoint(x: x, y: y)
+                }
+            }
+        }
+        print("END")
+        return CGPoint(x: -1, y: -1)
+    }
+    
+    private func locationInOriginalMask(size: CGSize, point: CGPoint) -> CGPoint {
+        var scale = 1.0
+        if size.width > size.height {
+            scale = maxDimension / size.width
+        } else {
+            scale = maxDimension / size.height
+        }
+        return CGPoint(x: Int(point.x / CGFloat(scale)), y: Int(point.y / CGFloat(scale)))
+    }
+    
+    private func downsampleSize(_ size: CGSize) -> CGSize {
+        if size.width > size.height {
+            let scale = maxDimension / size.width
+            return CGSize(width: maxDimension, height: size.height * scale)
+        } else {
+            let scale = maxDimension / size.height
+            return CGSize(width: size.width * scale, height: maxDimension)
+        }
+    }
+    
+    // This could be expensive
+    private func resize(image: UIImage, size: CGSize)-> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        let resizedImage = renderer.image { context in
+            image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+        }
+        return resizedImage
+    }
+    
     /* References
      WWDC session - https://developer.apple.com/videos/play/wwdc2018/416/?time=1373
      */
-    func downsample(image imageURL: URL,
-                    to size: CGSize,
-                    scale: CGFloat = UIScreen.main.scale) -> CGImage? {
+    private func downsample(image imageURL: URL, to size: CGSize, scale: CGFloat = UIScreen.main.scale) -> CGImage? {
         guard let imageSource = CGImageSourceCreateWithURL(imageURL as CFURL, nil) else {
             return nil
         }
@@ -260,7 +320,7 @@ extension ViewController {
         let options: [NSString: Any] = [
             kCGImageSourceCreateThumbnailWithTransform: true,
             kCGImageSourceCreateThumbnailFromImageAlways: true,
-            kCGImageSourceThumbnailMaxPixelSize: pixelDimension
+            kCGImageSourceThumbnailMaxPixelSize: pixelDimension,
         ]
         
         guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options as CFDictionary) else {
@@ -270,13 +330,13 @@ extension ViewController {
         return downsampledImage
     }
     
-    func createLocalURL(for image: UIImage, name: String, imageExtension: String = "jpg") -> URL? {
+    private func createLocalURL(for image: UIImage, name: String, imageExtension: String = "png") -> URL? {
         guard let imageURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("\(name).\(imageExtension)") else {
             return nil
         }
-        let pngData = image.jpegData(compressionQuality: 1)
+        let data = image.pngData()
         do {
-            try pngData?.write(to: imageURL);
+            try data?.write(to: imageURL);
         } catch {
             print(error)
             return nil
